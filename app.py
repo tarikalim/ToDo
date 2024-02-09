58f55696a5d5f043eed67cee2698a8fbc8eb04d3
tarikalim <tarikalim1899@gmail.com>
2024-02-09 11:35:13 +0300

    5 endpoint, mvc architecture and json format.

--- app.py
+++ app.py
@@ -1,48 +1,13 @@
-from flask import Flask, render_template
-from flask_login import LoginManager
-from flask_sqlalchemy import cli
-
-from controller import UserController, TodoController
-from models import db, User
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'your_secret_key_here'  # Flask-Login için gerekli olan gizli anahtar
+db = SQLAlchemy(app)
 
-db.init_app(app)
 
-# Flask-Login yapılandırması
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'  # Giriş yapılması gereken sayfanın adı (örneğin, login.html)
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-# Kullanıcı İşlemleri
-app.route('/register', methods=['GET', 'POST'])(UserController.register)
-app.route('/login', methods=['GET', 'POST'])(UserController.login)
-app.route('/logout')(UserController.logout)
-
-# Todo İşlemleri
-app.route('/todos', methods=['GET'])(TodoController.list_todos)
-app.route('/todos/add', methods=['GET', 'POST'])(TodoController.add_todo)
-app.route('/todos/<int:todo_id>/update', methods=['POST'])(TodoController.update_todo)
-app.route('/todos/<int:todo_id>/delete', methods=['POST'])(TodoController.delete_todo)
-
-@app.route('/')
-def index():
-    return render_template('login.html')
-
-@app.cli.command("create_db")
-def create_db():
-    db.create_all()
-
-@app.cli.command("drop_db")
-def drop_db():
-    db.drop_all()
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
